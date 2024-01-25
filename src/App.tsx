27493f4cab719af3be4fad917639d5324bfb03e4
yenny <yenny00001@163.com>
2024-01-25 14:40:24 +0800

    将头尾组件放入最顶层的APP，减少重复引用

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
+import Footer from './components/Footer.tsx';
+import Header from './components/Header.tsx';
+import Content from './views/Content/Content.tsx';
 import Home from './views/Home/index.tsx'
 import Personal from './views/Personal.tsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Personal" element={<Personal />} />
-      </Routes>
-    </Router>
+    <>
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Personal" element={<Personal />} />
+          <Route path="/Content" element={<Content />} />
+        </Routes>
+      </Router>
+      <Footer/>
+    </>
+
   )
 }
 
