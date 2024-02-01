fab1b138e642117a5728be16db2009b93d409859
yenny <yenny00001@163.com>
2024-02-01 16:39:41 +0800

    修复404页面跳转

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Footer from './components/Footer.tsx';
 import Header from './components/Header.tsx';
 import Content from './views/Content/Content.tsx';
 import Home from './views/Home/index.tsx'
-import Error from './views/Error.tsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound404 from './views/404.tsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,7 +13,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Error" element={<Error/>} />
+          <Route path="*" element={<Navigate to="/404" />} />
+          <Route path="/404" element={<NotFound404/>} />
           <Route path="/Content" element={<Content />} />
         </Routes>
       </Router>
