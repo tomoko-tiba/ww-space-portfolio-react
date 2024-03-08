c40b4a3238127a6a849b4215612da7bc245288e4
yenny <yenny00001@163.com>
2024-03-09 00:21:28 +0800

    实现富文本中的图片预览功能

--- src/views/Content/HtmlComp.tsx
+++ src/views/Content/HtmlComp.tsx
@@ -1,9 +1,22 @@
 import styles from './HtmpComp.module.less';
+import Viewer from 'viewerjs';
+import { useRef, useEffect } from 'react';
+import 'viewerjs/dist/viewer.css';
 interface HtmlComp {
   htmlContent: string;
 }
 function HtmlComp({ htmlContent }: HtmlComp) {
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} className={styles.htmlComp} />;
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const viewer = ref.current ? new Viewer(ref.current, {}) : null;
+    return () => {
+      viewer && viewer.destroy();
+    };
+  }, [ref]);
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} className={styles.htmlComp} ref={ref} />
+  );
 }
 
 export default HtmlComp;
