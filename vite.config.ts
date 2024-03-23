3f73e94667ccfa3e79b0164a7407eba12e0d7a70
yenny <yenny00001@163.com>
2024-03-23 15:23:18 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     proxy: {
-      "/backend/": {
-        target: "http://localhost:3000",
+      '/backend/': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
