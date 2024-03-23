a27d5a95786e5bc73c70a966f7ab5fc7f6278e4d
yenny <yenny00001@163.com>
2024-03-23 15:21:56 +0800

    修改路径为小写

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import "./App.css";
-import Content from "./views/Content/Content.tsx";
-import Home from "./views/Home/index.tsx";
-import NotFound404 from "./views/404.tsx";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Header from "./components/Header.tsx";
-import Footer from "./components/Footer.tsx";
-import { useState } from "react";
+import './App.css';
+import Content from './views/Content/Content.tsx';
+import Home from './views/Home/index.tsx';
+import NotFound404 from './views/404.tsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header.tsx';
+import Footer from './components/Footer.tsx';
+import { useState } from 'react';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchChange = (value: string) => {
     setSearchTerm(value);
@@ -29,7 +24,7 @@ function App() {
           <Route path="/Search/:text" element={<Home />} />
           <Route path="*" element={<Navigate to="/404" />} />
           <Route path="/404" element={<NotFound404 />} />
-          <Route path="/Content/:id" element={<Content />} />
+          <Route path="/content/:id" element={<Content />} />
         </Routes>
         <Footer />
       </Router>
